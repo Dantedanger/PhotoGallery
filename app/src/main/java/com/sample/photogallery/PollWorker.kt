6952ae4c3badb8a5011e57e39f9131c45c45820a
Diana <albinos55522@gmail.com>
2023-12-05 03:27:33 +0700

    4 лабораторная

--- app/src/main/java/com/sample/photogallery/PollWorker.kt
+++ app/src/main/java/com/sample/photogallery/PollWorker.kt
@@ -1,13 +1,17 @@
 package com.sample.photogallery
 
+import android.Manifest
 import android.app.PendingIntent
 import android.content.Context
-import android.support.v4.app.NotificationCompat
-import android.support.v4.app.NotificationManagerCompat
+import android.content.pm.PackageManager
 import android.util.Log
+import androidx.core.app.ActivityCompat
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import com.sample.photogallery.api.FlickrFetchr
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
 
 private const val TAG = "PollWorker"
 class PollWorker(val context: Context, workerParams: WorkerParameters)
@@ -40,29 +44,33 @@ class PollWorker(val context: Context, workerParams: WorkerParameters)
         } else {
             Log.i(TAG, "Got a new result:$resultId")
             QueryPreferences.setLastResultId(context, resultId)
-            val intent =
-                PhotoGalleryActivity.newIntent(context)
-            val pendingIntent =
-                PendingIntent.getActivity(context, 0, intent, 0)
-            val resources = context.resources
-            val notification =
-                NotificationCompat
-                    .Builder(context,
-                        NOTIFICATION_CHANNEL_ID)
-                    .setTicker(resources.getString(
-                        R.string.new_pictures_title))
-                    .setSmallIcon(android.R.drawable.ic_menu_report_image)
-                    .setContentTitle(resources.getString(R.string.new_pictures_title))
-                    .setContentText(resources.getString(R.string.new_pictures_text))
-                    .setContentIntent(pendingIntent
-                    )
-                    .setAutoCancel(true)
-                    .build()
-            val notificationManager =
-                NotificationManagerCompat.from(context)
-            notificationManager.notify(0, notification)
-
+            notifyUser()
         }
         return Result.success()
     }
+
+    private fun notifyUser() {
+        val intent = PhotoGalleryActivity.newIntent(context)
+        val pendingIntent = PendingIntent.getActivity(
+            context,
+            0,
+            intent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+        val resources = context.resources
+
+        val notification = NotificationCompat
+            .Builder(context, NOTIFICATION_CHANNEL_ID)
+            .setTicker(resources.getString(R.string.new_pictures_title))
+            .setSmallIcon(android.R.drawable.ic_menu_report_image)
+            .setContentTitle(resources.getString(R.string.new_pictures_title))
+            .setContentText(resources.getString(R.string.new_pictures_text))
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+            .build()
+
+        val notificationManager = NotificationManagerCompat.from(context)
+        notificationManager.notify(0, notification)
+    }
+
 }
