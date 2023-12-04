c6a67ff807bc133cf3ce047d0e054b1049170028
Diana <albinos55522@gmail.com>
2023-12-05 00:27:35 +0700

    реализация поиска

--- app/src/main/java/com/sample/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/sample/photogallery/PhotoGalleryFragment.kt
@@ -5,10 +5,12 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
@@ -90,7 +92,30 @@ class PhotoGalleryFragment : Fragment() {
         super.onCreateOptionsMenu(menu,
             inflater)
         inflater.inflate(R.menu.fragment_photo_gallery, menu)
+        val searchItem: MenuItem =
+            menu.findItem(R.id.menu_item_search)
+        val searchView = searchItem.actionView
+                as SearchView
+        searchView.apply {
+            setOnQueryTextListener(object :
+                SearchView.OnQueryTextListener {
+                override fun
+                        onQueryTextSubmit(queryText: String): Boolean {
+                    Log.d(TAG,
+                        "QueryTextSubmit: $queryText")
+                    photoGalleryViewModel.fetchPhotos(queryText)
+                    return true
+                }
+                override fun
+                        onQueryTextChange(queryText: String): Boolean {
+                    Log.d(TAG,
+                        "QueryTextChange: $queryText")
+                    return false
+                }
+            })
+        }
     }
+
 
     companion object {
         fun newInstance() = PhotoGalleryFragment()
--- app/src/main/java/com/sample/photogallery/PhotoGalleryViewModel.kt
+++ app/src/main/java/com/sample/photogallery/PhotoGalleryViewModel.kt
@@ -1,13 +1,24 @@
 package com.sample.photogallery
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.switchMap
 import com.sample.photogallery.api.FlickrFetchr
 
 class PhotoGalleryViewModel : ViewModel() {
     val galleryItemLiveData: LiveData<List<GalleryItem>>
+    private val flickrFetchr = FlickrFetchr()
+    private val mutableSearchTerm = MutableLiveData<String>()
+
     init {
-        galleryItemLiveData =
-            FlickrFetchr().searchPhotos("bicycle")
+        mutableSearchTerm.value = "planets"
+        galleryItemLiveData = mutableSearchTerm.switchMap { searchTerm ->
+                flickrFetchr.searchPhotos(searchTerm)
+            }
     }
+    fun fetchPhotos(query: String = "") {
+        mutableSearchTerm.value = query
+    }
+
 }
